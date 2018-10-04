474097013799e89fef41c95ab0aef21841dcc102
MMMayC <changingvan@gmail.com>
2018-10-04 21:35:42 +1000

    set up ssr

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+module.exports = {
+  entry: {
+    client: './src/client.js',
+    bundle: './src/bundle.js'
+  },
+  output: {
+    path: path.resolve(__dirname, 'public'),
+    filename: "[name].js"
+  },
+  module:{
+    rules: [
+        {
+            test:/\.js$/,
+            exclude:/node_modules/,
+            loader: 'babel-loader'
+        },
+        {
+            test: /\.less$/,
+            use: [
+              {
+                loader: "style-loader"
+              },
+              {
+                loader: "css-loader",
+                options: {
+                  sourceMap: true,
+                  modules: true,
+                  localIdentName: "[local]"
+                }
+              },
+              {
+                loader: "less-loader"
+              }
+            ]
+        }
+    ]
+}
+}
