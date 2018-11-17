bdf204bc136d08bd07dfcaaa1222df29aea98ced
MMMayC <changingvan@gmail.com>
2018-11-17 14:47:09 +1100

    vote swipe arrow working

--- src/components/votes.js
+++ src/components/votes.js
@@ -6,6 +6,8 @@ class Votes extends Component {
   constructor(props) {
     super(props);
     this.findCandidateByName = this.findCandidateByName.bind(this);
+    this.getNextVote = this.getNextVote.bind(this);
+    this.getPreviousVote = this.getPreviousVote.bind(this);
     this.state = {
       currentPage: 1
     }
@@ -14,22 +16,37 @@ class Votes extends Component {
     const candidate = this.props.candidates.filter(candidate => candidate.name == candidateName);
     return candidate[0];
   }
-    render(){
-      return (
-        <div className="Votes">
-          {
-            this.props.votes && this.props.votes !=[] ?
-            this.props.votes.map(vote => {
-              return(
-                <Vote vote={vote} candidate={this.findCandidateByName(vote.candidate)} />
-              )
-            }) 
-            : ""
-          }
-          <div className="Votes-Count">{this.state.currentPage} | {this.props.votes && this.props.votes != [] ? this.props.votes.length : "" }</div>
-        </div>
-      );
+  getNextVote() {
+    if(this.state.currentPage < this.props.votes.length) {
+      this.setState({
+        currentPage: ++ this.state.currentPage
+      });
     }
+  }
+  getPreviousVote() {
+    if(this.state.currentPage > 1) {
+      this.setState({
+        currentPage: -- this.state.currentPage
+      });
+    }
+  }
+  render(){
+    var currentVote = this.props.votes && this.props.votes != [] ? this.props.votes[this.state.currentPage - 1] : null;
+    return (
+      <div className="Votes">
+        {
+          currentVote ?
+            <Vote vote={currentVote} candidate={this.findCandidateByName(currentVote.candidate)} />
+            : ""
+        }
+        <div className="Votes-Count">{this.state.currentPage} | {this.props.votes && this.props.votes != [] ? this.props.votes.length : "" }</div>
+        <div className="Votes-Arrow">
+          <div className="Votes-Arrow-Left" onClick={this.getPreviousVote}><i class="fas fa-chevron-left"></i></div>
+          <div className="Votes-Arrow-Right" onClick={this.getNextVote}><i class="fas fa-chevron-right"></i></div>
+        </div>
+      </div>
+    );
+  }
 }
 
 function mapStateToProps(state) {
